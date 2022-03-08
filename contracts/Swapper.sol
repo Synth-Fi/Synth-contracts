8c8c11518728d85da0dc6ce8b5b2d5a06ef595b0
Daniel Leavitt <danielleavitt@Daniels-MacBook-Air-2.local>
2022-03-07 20:02:40 -0800

    Made swapper more robust

--- contracts/Swapper.sol
+++ contracts/Swapper.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity =0.7.6;
+pragma solidity ^0.7.0;
 pragma abicoder v2;
 
 import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
@@ -12,67 +12,77 @@ import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 */
 contract Swapper{
 
-  ISwapRouter public immutable swapRouter;
+  ISwapRouter public immutable swapRouter;  // Address of uniswap interface
 
-  address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-  address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-  address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+  address public token0;
+  address public token1;
+  uint24 public poolFee;
 
-  // For this example, we will set the pool fee to 0.3%.
-  uint24 public constant poolFee = 3000;
+  // address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+  // address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+  // address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+
+  // // For this example, we will set the pool fee to 0.3%.
+  // uint24 public constant poolFee = 3000;
 
   constructor(ISwapRouter _swapRouter) {
     swapRouter = _swapRouter;
   }
 
-  function swapExactInputSingle(address _token0, address _token1, uint256 _amountIn) external returns (uint256 amountOut) {
+  function swapExactInputSingle(address _token0, address _token1, uint24 _poolFee, uint256 _amountIn) external returns (uint256 amountOut) {
     //msg.sender must approve this contract
 
+    //Note: I don't think I need this if the vault will be calling it
     // Transfer the specified amount of token0 to this contract 
-    TransferHelper.safeTransferFrom(DAI, msg.sender, address(this), _amountIn);
+    // TransferHelper.safeTransferFrom(_token0, msg.sender, address(this), _amountIn);
 
-    TransferHelper.safeApprove(DAI, address(swapRouter), _amountIn);
+    // Approve the swap interface to take the vault's money
+    TransferHelper.safeApprove(_token0, address(swapRouter), _amountIn);
 
+    // swap structure
     ISwapRouter.ExactInputSingleParams memory params = 
       ISwapRouter.ExactInputSingleParams({
         tokenIn: _token0,
         tokenOut: _token1,
-        fee: poolFee,
+        fee: _poolFee,
         recipient: msg.sender,
         deadline: block.timestamp,
         amountIn: _amountIn,
-        amountOutMinimum: 0,
-        sqrtPriceLimitX96: 0
+        amountOutMinimum: 0,  //NOTE
+        sqrtPriceLimitX96: 0  //NOTE
       });
 
-      amountOut = swapRouter.exactInputSingle(params);
+      amountOut = swapRouter.exactInputSingle(params);  // Execute swap 
 
   }
 
-  function swapExactOutputsingle(address _token0, address _token1, uint256 _amountOut, uint256 _amountInMaximum) external returns (uint256 amountIn){
+  function swapExactOutputSingle(address _token0, address _token1, uint24 _poolFee, uint256 _amountOut, uint256 _amountInMaximum) external returns (uint256 amountIn){
 
+    //Note: Same as above
     // Transfer the specified amount of DIA to this contract
-    TransferHelper.safeTransferFrom(DAI, msg.sender, address(this), _amountInMaximum);
+    // TransferHelper.safeTransferFrom(_token0, msg.sender, address(this), _amountInMaximum);
 
-    TransferHelper.safeApprove(DAI, address(swapRouter), _amountInMaximum);
+    // Approve the swap interface to take the vault's money
+    TransferHelper.safeApprove(_token0, address(swapRouter), _amountInMaximum);
 
+    // Swap structure
     ISwapRouter.ExactOutputSingleParams memory params = 
       ISwapRouter.ExactOutputSingleParams({
         tokenIn: _token0,
         tokenOut: _token1,
-        fee: poolFee,
+        fee: _poolFee,
         recipient: msg.sender,
         deadline: block.timestamp,
         amountOut: _amountOut,
-        amountInMaximum: _amountInMaximum,
-        sqrtPriceLimitX96: 0
+        amountInMaximum: _amountInMaximum,  //Note: need to decide this
+        sqrtPriceLimitX96: 0  //NOTE
       });
 
-    amountIn = swapRouter.exactOutputSingle(params);
+    amountIn = swapRouter.exactOutputSingle(params);  // Execute swap 
 
     if(amountIn < _amountInMaximum){
       TransferHelper.safeApprove(_token0, address(swapRouter), 0);
-      TransferHelper.safeTransfer(_token0, msg.sender, _amountInMaximum - amountIn);
+      //TransferHelper.safeTransfer(_token0, msg.sender, _amountInMaximum - amountIn);
     }
   }
 
